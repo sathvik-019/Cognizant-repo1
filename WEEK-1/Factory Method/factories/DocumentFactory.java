b6f720eaf779e084c36b78ba5e5b36712a55ecf1
Sai Sathvik Peddireddy <saipeddireddy9@gmail.com>
2025-06-22 15:32:36 +0530

    Add files via upload

---
+++ WEEK-1/Factory Method/factories/DocumentFactory.java
@@ -0,0 +1,6 @@
+package factories;
+import documents.Document;
+
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
---
+++ WEEK-1/Factory Method/factories/WordDocumentFactory.java
@@ -0,0 +1,13 @@
+package factories;
+
+import documents.Document;
+import documents.WordDocument;
+
+public class WordDocumentFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+        return new WordDocument();
+    }
+    
+}
